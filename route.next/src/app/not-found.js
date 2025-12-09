8d547d0a2a408e06723898abc4a55209992c1f83
Riccardo V. <ricc.vargiu@gmail.com>
2025-12-09 16:31:26 +0100

    feat: Enhance NotFound component with image display for better user experience

--- route.next/src/app/not-found.js
+++ route.next/src/app/not-found.js
@@ -1,3 +1,10 @@
+import Image from "next/image";
+
 export default function NotFound() {
-  return <p>The page you are looking for does not exist.</p>;
+  return (
+    <>
+      <p>The page you are looking for does not exist.</p>
+      <Image src="/file.svg" alt="Not Found" width={400} height={300} />
+    </>
+  );
 }
